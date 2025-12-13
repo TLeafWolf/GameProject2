bbb4c0d47b6f051306825867e63f9bcfb1f1396e
Aaron Roberts <118574690+BurntToast2424@users.noreply.github.com>
2025-12-13 15:53:20 -0500

    Updated enemy

--- game-project-2/enemy.gd
+++ game-project-2/enemy.gd
@@ -5,8 +5,18 @@ extends CharacterBody3D
 
 const SPEED = 3.0
 const STOP_DISTANCE = 0.1
+var angry = false
 
 func _physics_process(delta: float) -> void:
+	if !angry:
+		return
+	
+	# rotate to player
+	look_at(player.position)
+	rotation.x = 0
+	rotation.y += 3.14159
+	rotation.z = 0
+	
 	if nav.is_navigation_finished():
 		velocity = Vector3.ZERO
 		move_and_slide()
@@ -30,13 +40,18 @@ func _physics_process(delta: float) -> void:
 func _set_target() -> void:
 	nav.set_target_position(player.position)
 
-func _on_area_3d_body_entered(body: Node3D) -> void:
+func _on_attack_box_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		$AttackTimer.start()
 
-func _on_area_3d_body_exited(body: Node3D) -> void:
+func _on_attack_box_exited(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		$AttackTimer.stop()
 
 func _on_attack_timer_timeout() -> void:
+	$Goblin/AnimationPlayer.play("Cylinder_003Action")
 	player.take_damage(5)
+
+func _on_aggro_range_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		angry = true
